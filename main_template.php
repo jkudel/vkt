453b3bf2084a997745b92bd90d4489cdd1dd5403
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2015-07-22 01:45:05 +0300

    title prefix

--- main_template.php
+++ main_template.php
@@ -2,7 +2,7 @@
 <html>
 <head lang="en">
   <meta charset="UTF-8">
-  <title><?= $title ?></title>
+  <title>VKT | <?= $title ?></title>
   <link rel="stylesheet" href="styles.css" type="text/css"/>
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <script type="text/javascript" src="js/moment.min.js"></script>
